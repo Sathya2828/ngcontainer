59bdc8f0d98903fe69751eb36e9d420b5c28ed47
sathya <besthasathyanarayana28@gmail.com>
2023-06-21 20:20:42 +0530

    ngContainer

---
+++ container/src/app/cont/cont.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cont',
+  templateUrl: './cont.component.html',
+  styleUrls: ['./cont.component.css']
+})
+export class ContComponent implements OnInit{
+  numbers=[1,2,3,4,5,6,7,8,9,10];
+  ngOnInit() {
+  
+  }
+
+}
